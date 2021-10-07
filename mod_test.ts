7daf4b97c71c8d80229cec4da2ad9018917f7ff6
Peter Batory-Bernardin <peter.bernardin@gmail.com>
2021-10-07 10:07:26 -0400

    Added performance benchmark test

--- mod_test.ts
+++ mod_test.ts
@@ -22,7 +22,28 @@ Deno.test("Error test", async () => {
 
 // This test won't fail if it doesn't work. Check the console log returns to make sure that it's not detecting
 Deno.test("Interference test", async () => {
-    // await some async task
-    worker.postMessage('Not promise Marco ')
-
+    worker.postMessage('Not promise Marco')
   });
+
+Deno.test("Performance test", async () => {
+    const longstring = 'the quick brown fox jumped over'.repeat(1000000) // ~2.5 ish kb
+    let t0 = performance.now()
+    const performanceworker = new PromiseWorker(new URL("./test_worker.ts", import.meta.url).href, { type: "module" })
+    let t1 = performance.now()
+    console.log('Spawning promise worker: ', t1 - t0, 'ms')
+
+    t0 = performance.now()
+    await performanceworker.sendPromise('Marco 1 ')
+    t1 = performance.now()
+    console.log('Simple string: ', t1 - t0, 'ms')
+
+    t0 = performance.now()
+    await performanceworker.sendPromise({str: 'Marco 2 ', str2: longstring})
+    t1 = performance.now()
+    console.log('Long string promise: ', t1 - t0, 'ms')
+
+    t0 = performance.now()
+    await performanceworker.sendPromise('Marco 3 ')
+    t1 = performance.now()
+    console.log('Sending third promise: ', t1 - t0, 'ms')
+})
